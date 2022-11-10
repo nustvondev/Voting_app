91859374ed4fad2cb9dd6d164223a5606531c83d
Vjet Node JS <hoaboy89@gmail.com>
2022-11-10 08:24:41 +0000

    Update complete server.

--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -1,43 +1,75 @@
-import { Request } from '@nestjs/common';
+import { Request } from 'express';
+import { Nomination } from 'shared/poll-types';
+import { Socket } from 'socket.io';
 
-// types service
+// service types
 export type CreatePollFields = {
-    topic: string;
-    votesPerVoter: number;
-    name: string;
+  topic: string;
+  votesPerVoter: number;
+  name: string;
 };
 
 export type JoinPollFields = {
-    pollID: string;
-    name: string;
+  pollID: string;
+  name: string;
 };
 
 export type RejoinPollFields = {
-    pollID: string;
-    userID: string;
-    name: string;
+  pollID: string;
+  userID: string;
+  name: string;
 };
 
-// types repository 
+export type AddParticipantFields = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
+
+export type AddNominationFields = {
+  pollID: string;
+  userID: string;
+  text: string;
+};
+
+export type SubmitRankingsFields = {
+  pollID: string;
+  userID: string;
+  rankings: string[];
+};
+
+// repository types
 export type CreatePollData = {
-    pollID: string;
-    topic: string;
-    votesPerVoter: number;
-    userID: string;
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
 };
 
 export type AddParticipantData = {
-    pollID: string;
-    userID: string;
-    name: string;
+  pollID: string;
+  userID: string;
+  name: string;
 };
 
+export type AddNominationData = {
+  pollID: string;
+  nominationID: string;
+  nomination: Nomination;
+};
+
+export type AddParticipantRankingsData = {
+  pollID: string;
+  userID: string;
+  rankings: string[];
+};
 
 // guard types
-type AuthPayload = {
-    userID: string;
-    pollID: string;
-    name: string;
+export type AuthPayload = {
+  userID: string;
+  pollID: string;
+  name: string;
 };
 
 export type RequestWithAuth = Request & AuthPayload;
+export type SocketWithAuth = Socket & AuthPayload;
