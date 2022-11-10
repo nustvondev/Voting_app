4f2278b1cbcb8b6466e209f21fc410d1ab55c192
Vjet Node JS <hoaboy89@gmail.com>
2022-11-10 12:07:06 +0000

    Final project.

--- client/src/Pages.tsx
+++ client/src/Pages.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { useSnapshot } from 'valtio';
 import Create from './pages/Create';
 import Join from './pages/Join';
+import { Results } from './pages/Results';
+import { Voting } from './pages/Voting';
+import { WaitingRoom } from './pages/WaitingRoom';
 import Welcome from './pages/Welcome';
-import { AppPage, state } from './state';
+import { actions, AppPage, state } from './state';
 
 const routeConfig = {
   [AppPage.Welcome]: Welcome,
   [AppPage.Create]: Create,
   [AppPage.Join]: Join,
+  [AppPage.WaitingRoom]: WaitingRoom,
+  [AppPage.Voting]: Voting,
+  [AppPage.Results]: Results,
 };
 
 const Pages: React.FC = () => {
   const currentState = useSnapshot(state);
+
+  useEffect(() => {
+    if (
+      currentState.me?.id &&
+      currentState.poll &&
+      !currentState.poll?.hasStarted
+    ) {
+      actions.setPage(AppPage.WaitingRoom);
+    }
+
+    if (currentState.me?.id && currentState.poll?.hasStarted) {
+      actions.setPage(AppPage.Voting);
+    }
+
+    if (currentState.me?.id && currentState.hasVoted) {
+      actions.setPage(AppPage.Results);
+    }
+  }, [
+    currentState.me?.id,
+    currentState.poll?.hasStarted,
+    currentState.hasVoted,
+  ]);
+
   return (
     <>
       {Object.entries(routeConfig).map(([page, Component]) => (
